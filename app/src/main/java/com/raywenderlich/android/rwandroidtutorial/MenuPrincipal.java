8076052df57ccf1aaa7e1b55010c917afd0a1e4a
alexjejej <alejandro.garana@alumnos.udg.mx>
2022-11-03 14:51:09 -0600

    first commit alexgg

---
+++ app/src/main/java/com/raywenderlich/android/rwandroidtutorial/MenuPrincipal.java
@@ -0,0 +1,24 @@
+package com.raywenderlich.android.rwandroidtutorial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.raywenderlich.android.rwandroidtutorial.MapaySteps.MapsActivity;
+import com.raywenderlich.android.runtracking.R;
+
+public class MenuPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_principal);
+    }
+    public void btnCarrera(View view) {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+    }
+
+}
